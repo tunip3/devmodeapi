255422c32f4fa89ae171a76e80ef71cd2e753a82
tunip3 <26260613+tunip3@users.noreply.github.com>
2020-01-19 11:01:34 +0000

    add user account control and some other stuff

--- devmodeapi.py
+++ devmodeapi.py
@@ -2,7 +2,9 @@ import sys
 import datetime
 import requests
 import urllib3
+from urllib.parse import quote_plus
 from base64 import b64encode
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -32,27 +34,25 @@ class XboxOneDevmodeApi(object):
         return self.session.put(self.base_url + endpoint, headers=self._csrf_header, *args, **kwargs)
 
     def _delete(self, endpoint, *args, **kwargs):
-        return self.session.delete(self.base_url + endpoint, *args, **kwargs)
+        return self.session.delete(self.base_url + endpoint, headers=self._csrf_header, *args, **kwargs)
 
     def set_credentials(self, user, pwd):
         self.session.auth = (user, pwd)
 
     def get_root(self):
         return self._get('/')
-		
+
     def launchapp(self, relativeappid):
-        rai = str(b64encode(relativeappid.encode()))
-        rai = rai[2:-1]
-        rai = rai.replace("=", "%3D")
-        url="/api/taskmanager/app?appid="+rai
-        return self._post(url)
+        rai = b64encode(relativeappid.encode('utf8')).decode('utf8')
+        rai = quote_plus(rai)
+        url = "/api/taskmanager/app?appid=" + rai
+        return self._post(url).status_code
 
     def setmachinename(self, name):
-        name = str(b64encode(name.encode()))
-        name = name[2:-1]
-        name = name.replace("=", "%3D")
-        url="/api/os/machinename?name="+name
-        return self._post(url)
+        name = b64encode(name.encode('utf8')).decode('utf8')
+        name = quote_plus(name)
+        url = "/api/os/machinename?name=" + name
+        return self._post(url).status_code
 
     def reboot(self):
         return self._post('/api/control/restart')
@@ -62,15 +62,15 @@ class XboxOneDevmodeApi(object):
 
     def install(self, appx):
         files = {'upload_file': appx}
-        filename=str(appx)
-        filename=filename[26:-2]
-        url="/api/app/packagemanager/package?package="+filename
+        filename = str(appx)
+        filename = filename[26:-2]
+        url = "/api/app/packagemanager/package?package=" + filename
         return self._post(url, files=files)
 
     def get_isproxyenabled(self):
         family = self._get('/ext/fiddler ').json()
         return family.get('IsProxyEnabled') == 'true'
-    
+
     def get_knownfolders(self):
         family = self._get('/api/filesystem/apps/knownfolders').json()
         return family.get('KnownFolders')
@@ -82,6 +82,9 @@ class XboxOneDevmodeApi(object):
     def get_connectedcontrollercount(self):
         controllers = self._get('/ext/remoteinput/controllers').json()
         return controllers.get('ConnectedControllerCount')
+
+    def disconnect_controllers(self):
+        return self._delete('/ext/remoteinput/controllers')
 
     def get_machinename(self):
         machine = self._get('/api/os/machinename').json()
@@ -154,17 +157,17 @@ class XboxOneDevmodeApi(object):
 
     def _get_smbinfo(self):
         return self._get('/ext/smb/developerfolder').json()
-    
+
     def get_smbpath(self):
-        smbinfo =  self._get_smbinfo()
+        smbinfo = self._get_smbinfo()
         return smbinfo.get('Path')
 
     def get_smbusername(self):
-        smbinfo =  self._get_smbinfo()
+        smbinfo = self._get_smbinfo()
         return smbinfo.get('Username')
 
     def get_smbpassword(self):
-        smbinfo =  self._get_smbinfo()
+        smbinfo = self._get_smbinfo()
         return smbinfo.get('Password')
 
     def _get_processes(self):
@@ -175,7 +178,7 @@ class XboxOneDevmodeApi(object):
         return processlist.get('Processes')
 
     def get_processnames(self):
-        names=[]
+        names = []
         for i in self._get_processlist():
             proc = i.get('ImageName')
             if proc != '':
@@ -183,7 +186,7 @@ class XboxOneDevmodeApi(object):
         return names
 
     def get_pidsfromprocessname(self, name):
-        pids=[]
+        pids = []
         for i in self._get_processlist():
             procname = i.get('ImageName')
             if procname == name:
@@ -216,12 +219,23 @@ class XboxOneDevmodeApi(object):
         remove = self._delete('/ext/app/sshpins')
         return remove.status_code
 
+    def add_newaccount(self, email, password, autosignin):
+        url = "/ext/user"
+        json = {"Users": [
+            {"AutoSignIn": autosignin, "EmailAddress": str(email), "Password": str(password), "SignedIn": True}]}
+        return self._put(url, json=json).status_code
+
+    def delete_account(self, email):
+        url = "/ext/user"
+        json = {"Users": [{"EmailAddress": str(email), "Delete": True}]}
+        return self._put(url, json=json).status_code
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('ERROR: Please provide IP address')
         print('Usage: {0} <ip> <username> <password>'.format(sys.argv[0]))
         sys.exit(1)
-    
+
     ip_address = sys.argv[1]
     api = XboxOneDevmodeApi(ip_address)
 
@@ -235,7 +249,7 @@ if __name__ == '__main__':
         print('ERROR: Authentication failed, HTTP Status: {0}'.format(r.status_code))
         sys.exit(2)
 
-    print("Is proxy enabled : {0}".format(api.get_isproxyenabled()))	
+    print("Is proxy enabled : {0}".format(api.get_isproxyenabled()))
     print("Folders in top directory : {0}".format(api.get_knownfolders()))
     print('ConsoleId: {0}'.format(api.get_consoleid()))
     print('ConsoleType: {0}'.format(api.get_consoletype()))
@@ -258,15 +272,16 @@ if __name__ == '__main__':
     print('Name of the process with pid 0: {0}'.format(api.get_processnamefrompid(0)))
     print('User currently running the process with pid 0: {0}'.format(api.get_usernamefrompid(0)))
     print('CPU usage of the process with pid 0: {0}%'.format(api.get_cpuusagefrompid(0)))
-    #print('Status code from deleting trusted ssh pins: {0}'.format(api.remove_trustedsshpins()))
-    #print('currently running processes:')
-    #for i in api.get_processnames():
+    # api.disconnect_controllers()
+    # print('Status code from deleting trusted ssh pins: {0}'.format(api.remove_trustedsshpins()))
+    # print('currently running processes:')
+    # for i in api.get_processnames():
     #    print(i)
-    #print('pids for sshd.exe')
-    #for i in apik.get_pidsfromprocessname('sshd.exe'):
+    # print('pids for sshd.exe')
+    # for i in api.get_pidsfromprocessname('sshd.exe'):
     #    print(i)
     api.launchapp('DefaultApp_cw5n1h2txyewy!App')
-    #this works just doesnt show up in the dev menu app
-    #api.setmachinename('XBOXONE')
-    #print('Setting: {0}'.format(api.get_setting('DefaultUWPContentTypeToGame')))
-	# api.reboot()
+    # this works just doesnt show up in the dev menu app
+    # api.setmachinename('XBOXONE')
+    # print('Setting: {0}'.format(api.get_setting('DefaultUWPContentTypeToGame')))
+# api.reboot()
